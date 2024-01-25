09f0eeef54525c1195b384b16eb566b68f0418e6
Argenis Osorio <argenisosorio580@hotmail.com>
2024-01-25 15:44:59 -0400

    Updated navbar options

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -48,36 +48,60 @@ import { Head, Link } from '@inertiajs/vue3';
             >
                 <ul class="list-reset lg:flex justify-end flex-1 items-center">
                     <li class="mr-3">
-                    <a
-                        class="inline-block py-2 px-4 text-black font-bold no-underline"
-                        href="/"
-                    >
-                        Inicio
-                    </a>
+                        <a
+                            class="inline-block py-2 px-4 text-black font-bold no-underline"
+                            href="/"
+                        >
+                            Inicio
+                        </a>
                     </li>
                     <li class="mr-3">
-                    <a
-                        class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                        href="#"
-                    >
-                        link
-                    </a>
+                        <a
+                            class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                            href="#"
+                        >
+                            Acerca de nosotros
+                        </a>
                     </li>
                     <li class="mr-3">
-                    <a
-                        class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                        href="#"
-                    >
-                        link
-                    </a>
+                        <a
+                            class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                            href="#"
+                        >
+                            Productos
+                        </a>
+                    </li>
+                    <li class="mr-3">
+                        <a
+                            class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                            href="#"
+                        >
+                            Servicios
+                        </a>
+                    </li>
+                    <li class="mr-3">
+                        <a
+                            class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                            href="#"
+                        >
+                            Blog
+                        </a>
+                    </li>
+                    <li class="mr-3">
+                        <a
+                            class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                            href="#"
+                        >
+                            Contacto
+                        </a>
                     </li>
                 </ul>
-                <button
+                <!--button
                     id="navAction"
                     class="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded mt-4 lg:mt-0 py-4 px-8 shadow opacity-75"
                 >
                     Action
-                </button>
+                </button-->
             </div>
         </div>
     </nav>
