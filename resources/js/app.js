78d60e65758941569596710166592737b1b8e52c
Argenis Osorio <argenisosorio580@hotmail.com>
2024-01-29 16:20:00 -0400

    Moved app.blade.php template scripts to main js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,3 +22,41 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+/**
+ * Métodos que permiten desplegar el menú cuando está en modo mobile.
+*/
+
+var navMenuDiv = document.getElementById("nav-content");
+var navMenu = document.getElementById("nav-toggle");
+
+document.onclick = check;
+function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+
+    // Nav Menu
+    if (!checkParent(target, navMenuDiv)) {
+        // click NOT on the menu
+        if (checkParent(target, navMenu)) {
+            // click on the link
+            if (navMenuDiv.classList.contains("hidden")) {
+                navMenuDiv.classList.remove("hidden");
+            } else {
+                navMenuDiv.classList.add("hidden");
+            }
+        } else {
+            // click both outside link and outside menu, hide menu
+            navMenuDiv.classList.add("hidden");
+        }
+    }
+}
+
+function checkParent(t, elm) {
+    while (t.parentNode) {
+        if (t == elm) {
+            return true;
+        }
+        t = t.parentNode;
+    }
+    return false;
+}
