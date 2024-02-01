620f759d9854bc43e6e4f46941fcd1b5c788ac36
Argenis Osorio <argenisosorio580@hotmail.com>
2024-01-31 23:26:03 -0400

    Fix styles in pending order detail page

--- resources/js/Pages/PendingOrder/Detail.vue
+++ resources/js/Pages/PendingOrder/Detail.vue
@@ -1,24 +1,39 @@
 <template>
-    <h1>Orden Pendiente</h1>
-    <h2>Datos personales</h2>
-    <h3>Titular:</h3>
-    <p>{{ `${pendingOrder.owner_firstname} ${pendingOrder.owner_lastname}` }}</p>
-    <h3>Contactos:</h3>
-    <p><strong>E-mail: </strong>{{ `${pendingOrder.owner_email}` }}</p>
-    <p><strong>Teléfono: </strong>{{ `${pendingOrder.owner_phone_number}` }}</p>
-    <h3>Dirección:</h3>
-    <p>{{ `${pendingOrder.owner_state}, ${pendingOrder.owner_city}, ${pendingOrder.owner_address}` }}</p>
-    <h2>Datos de la orden</h2>
-    <h3>Pedido:</h3>
-    <p>{{ pendingOrder.owner_request }}</p>
-    <h3>Código:</h3>
-    <p>{{ pendingOrder.id }}</p>
-    <h3>Estado:</h3>
-    <p>En espera</p>
+    <Head title="Detalle de la orden" />
+
+    <NavBar />
+
+    <section class="bg-white border-b py-12">
+        <div class="container mx-auto">
+            <h2 class="text-3xl font-bold text-center text-gray-800 mb-8">
+                Detalle de la orden
+            </h2>
+            <br>
+            <h1>Orden Pendiente</h1>
+            <h2>Datos personales</h2>
+            <h3>Titular:</h3>
+            <p>{{ `${pendingOrder.owner_firstname} ${pendingOrder.owner_lastname}` }}</p>
+            <h3>Contactos:</h3>
+            <p><strong>E-mail: </strong>{{ `${pendingOrder.owner_email}` }}</p>
+            <p><strong>Teléfono: </strong>{{ `${pendingOrder.owner_phone_number}` }}</p>
+            <h3>Dirección:</h3>
+            <p>{{ `${pendingOrder.owner_state}, ${pendingOrder.owner_city}, ${pendingOrder.owner_address}` }}</p>
+            <h2>Datos de la orden</h2>
+            <h3>Pedido:</h3>
+            <p>{{ pendingOrder.owner_request }}</p>
+            <h3>Código:</h3>
+            <p>{{ pendingOrder.id }}</p>
+            <h3>Estado:</h3>
+            <p>En espera</p>
+        </div>
+    </section>
 </template>
 
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import NavBar from '@/Layouts/NavBar.vue';
+import { onMounted, ref } from 'vue';
+import { Head, Link } from '@inertiajs/vue3';
+
 const props = defineProps({
     pendingOrder: { type: Object, required: true },
 });
