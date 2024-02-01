203434b6edc0e569bbc495215eb6b08fc373d6b8
Argenis Osorio <argenisosorio580@hotmail.com>
2024-01-31 23:38:50 -0400

    Added documentation to methods. Fix styles in errors component and new order form

--- resources/js/Components/ErrorList.vue
+++ resources/js/Components/ErrorList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div role="alert">
+    <div class="container mx-auto" role="alert">
         <button data-dismiss="alert" aria-label="Cerrar" @click.prevent="handleClearErrorsClick"
             class="!absolute  top-3 right-3 h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-white transition-all hover:bg-white/10 active:bg-white/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button">
@@ -11,8 +11,16 @@
             </span>
         </button>
         <ul>
-            <li class="relative block w-full p-4 mb-4 text-base leading-5 text-white bg-red-500 rounded-lg opacity-100 font-regular"
-                v-for="(error, index) in errors" key="index">{{ error }}</li>
+            <li
+                class="
+                    relative block w-full p-4 mb-4 text-base leading-5
+                    text-white bg-red-500 rounded-lg opacity-100 font-regular
+                "
+                v-for="(error, index) in errors"
+                key="index"
+            >
+                {{ error }}
+            </li>
         </ul>
     </div>
 </template>
--- resources/js/Pages/PendingOrder/Form.vue
+++ resources/js/Pages/PendingOrder/Form.vue
@@ -4,7 +4,11 @@
     <NavBar />
 
     <section class="bg-white border-b py-12">
-        <ErrorList v-if="errors.length > 0" :errors="errors" @clear-errors="clearErrors" />
+        <ErrorList
+            v-if="errors.length > 0"
+            :errors="errors"
+            @clear-errors="clearErrors"
+        />
         <div class="container mx-auto p-4">
             <h2 class="text-3xl font-bold text-center text-gray-800 mb-8">
                 Formulario de Registro
@@ -13,34 +17,63 @@
             <form @submit.prevent="submitForm" class="max-w-md mx-auto bg-white p-8 rounded shadow-md">
                 <!-- Nombre del titular -->
                 <div class="mb-4">
-                    <label for="owner_firstname" class="block text-gray-700 text-sm font-bold mb-2">Nombre del
-                        Titular:</label>
-                    <input v-model="pendingOrder.owner_firstname" type="text" id="owner_firstname" name="owner_firstname"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.nombre" class="text-red-500 text-sm">{{ errors.nombre }}</span> -->
+                    <label
+                        for="owner_firstname"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Nombre del Titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_firstname"
+                        type="text"
+                        id="owner_firstname"
+                        name="owner_firstname"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <!-- Apellido del titular -->
                 <div class="mb-4">
-                    <label for="owner_lastname" class="block text-gray-700 text-sm font-bold mb-2">Apellido del
-                        Titular:</label>
-                    <input v-model="pendingOrder.owner_lastname" type="text" id="owner_lastname" name="owner_lastname"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.apellido" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <label
+                        for="owner_lastname"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Apellido del Titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_lastname"
+                        type="text"
+                        id="owner_lastname"
+                        name="owner_lastname"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <!-- Cédula de identidad del titular -->
                 <div class="mb-4">
-                    <label for="owner_id" class="block text-gray-700 text-sm font-bold mb-2">Cédula de Identidad:</label>
-                    <input v-model="pendingOrder.owner_id" type="text" id="owner_id" name="owner_id"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.cedula" class="text-red-500 text-sm">{{ errors.cedula }}</span> -->
+                    <label
+                        for="owner_id"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Cédula de Identidad:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_id"
+                        type="text"
+                        id="owner_id"
+                        name="owner_id"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <!-- Número de teléfono del titular -->
                 <div class="mb-4 flex items-center">
-                    <label for="owner_phone_number" class="block text-gray-700 text-sm font-bold mb-2">Número de teléfono
-                        del titular:</label>
+                    <label
+                        for="owner_phone_number"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Número de teléfono del titular:
+                    </label>
 
                     <!-- Selector de código de área -->
                     <select class="mr-2 px-3 py-2 border rounded">
@@ -49,54 +82,101 @@
                     </select>
 
                     <!-- Campo de número de teléfono -->
-                    <input v-model="pendingOrder.owner_phone_number" type="text" id="owner_phone_number"
-                        name="owner_phone_number" class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.cedula" class="text-red-500 text-sm">{{ errors.cedula }}</span> -->
-                </div>
-
-
-                <div class="mb-4">
-                    <label for="owner_email" class="block text-gray-700 text-sm font-bold mb-2">Correo electrónico del
-                        Titular:</label>
-                    <input v-model="pendingOrder.owner_email" type="text" id="owner_email" name="owner_email"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.email" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <input
+                        v-model="pendingOrder.owner_phone_number"
+                        type="text" id="owner_phone_number"
+                        name="owner_phone_number"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <div class="mb-4">
-                    <label for="owner_state" class="block text-gray-700 text-sm font-bold mb-2">Estado de procedencia del
-                        titular:</label>
-                    <input v-model="pendingOrder.owner_state" type="text" id="owner_state" name="owner_state"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.owner_state" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <label
+                        for="owner_email"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Correo electrónico del Titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_email"
+                        type="text"
+                        id="owner_email"
+                        name="owner_email"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <div class="mb-4">
-                    <label for="owner_city" class="block text-gray-700 text-sm font-bold mb-2">Ciudad de procedencia del
-                        titular:</label>
-                    <input v-model="pendingOrder.owner_city" type="text" id="owner_city" name="owner_city"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.owner_city" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <label
+                        for="owner_state"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Estado de procedencia del titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_state"
+                        type="text" id="owner_state"
+                        name="owner_state"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <div class="mb-4">
-                    <label for="owner_address" class="block text-gray-700 text-sm font-bold mb-2">Dirección de domicilio del
-                        titular:</label>
-                    <input v-model="pendingOrder.owner_address" type="text" id="owner_address" name="owner_address"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.owner_address" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <label
+                        for="owner_city"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Ciudad de procedencia del titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_city"
+                        type="text"
+                        id="owner_city"
+                        name="owner_city"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <div class="mb-4">
-                    <label for="owner_request" class="block text-gray-700 text-sm font-bold mb-2">¿Qué desea?</label>
-                    <input v-model="pendingOrder.owner_request" type="text" id="owner_request" name="owner_request"
-                        class="w-full px-3 py-2 border rounded">
-                    <!-- <span v-if="errors.owner_request" class="text-red-500 text-sm">{{ errors.apellido }}</span> -->
+                    <label
+                        for="owner_address"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        Dirección de domicilio del titular:
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_address"
+                        type="text"
+                        id="owner_address"
+                        name="owner_address"
+                        class="w-full px-3 py-2 border rounded"
+                    >
+                </div>
+
+                <div class="mb-4">
+                    <label
+                        for="owner_request"
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                        ¿Qué desea?
+                    </label>
+                    <input
+                        v-model="pendingOrder.owner_request"
+                        type="text"
+                        id="owner_request"
+                        name="owner_request"
+                        class="w-full px-3 py-2 border rounded"
+                    >
                 </div>
 
                 <!-- Botón de enviar -->
                 <div class="mt-6">
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    <button
+                        type="submit"
+                        class="
+                            bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600
+                        "
+                    >
                         Enviar
                     </button>
                 </div>
@@ -128,19 +208,23 @@ const errors = ref([]);
 
 /**
  * Método que desplaza la pantalla a la cabecera del formulario.
- *
- * @author Argenis Osorio <aosorio@cenditel.gob.ve>
  */
 const scrollMeTo = () => {
     const top = form.offsetTop;
     window.scrollTo(0, top);
-    // window.scrollTo({ top: form.offsetTop, left: 0, behavior: "smooth" });
 }
 
+/**
+ * Método que limpia los errores del formulario.
+ */
 const clearErrors = () => {
     errors.value = []
 }
 
+/**
+ * Método que envía al api los datos del formulario y redirecciona a la vista
+ * que detalla el registro realizado.
+ */
 const submitForm = async () => {
     try {
         const response = await axios.post('/api/pending-orders', pendingOrder.value);
