0188999edbbf7c2ced9121c00edb704f582845c6
Argenis Osorio <argenisosorio580@hotmail.com>
2024-02-08 20:58:50 -0400

    Updated texts of Products and Services un landing page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -169,26 +169,91 @@ onMounted(async () => {
                     <div class="flex flex-wrap">
                         <div class="sm:w-1/2 p-6">
                             <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
-                                Lorem ipsum dolor sit amet
+                                Planificación y diseño de cosméticos
                             </h3>
                             <p class="text-gray-600">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                at ipsum eu nunc commodo posuere et sit amet ligula.
+                                ¡Anímate. Que sólo la intención ya no cuenta en
+                                éstos tiempos! Materializa con nosotros tus
+                                ideas y conviértelas en productos
+                                personalizados, a gusto tuyo o de tus clientes.
                             </p>
                         </div>
                         <div class="sm:w-1/2">
                             <img :src="productOne" class="w-80 h-auto mx-auto">
                         </div>
                     </div>
-
+                    <hr>
                     <div class="flex flex-wrap">
                         <div class="sm:w-1/2 p-6">
                             <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
-                                Lorem ipsum dolor sit amet
+                                Fabricación a granel
                             </h3>
                             <p class="text-gray-600">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                at ipsum eu nunc commodo posuere et sit amet ligula.
+                                Cuenta con nosotros para fabricar a escala,
+                                aquello que deseas.
+                            </p>
+                        </div>
+                        <div class="sm:w-1/2">
+                            <img :src="productOne" class="w-80 h-auto mx-auto">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="flex flex-wrap">
+                        <div class="sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
+                                Asesorías
+                            </h3>
+                            <p class="text-gray-600">
+                                Te orientamos en todas las etapas previas a
+                                comercializar tus productos o marca.
+                            </p>
+                        </div>
+                        <div class="sm:w-1/2">
+                            <img :src="productOne" class="w-80 h-auto mx-auto">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="flex flex-wrap">
+                        <div class="sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
+                                Análisis de muestras
+                            </h3>
+                            <p class="text-gray-600">
+                                Si tienes el producto y desear registros de las
+                                propiedades químicas (pH, viscosidad, grado de
+                                alcohol, conductividad), también podemos
+                                ayudarte.
+                            </p>
+                        </div>
+                        <div class="sm:w-1/2">
+                            <img :src="productOne" class="w-80 h-auto mx-auto">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="flex flex-wrap">
+                        <div class="sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
+                                Pulverización
+                            </h3>
+                            <p class="text-gray-600">
+                                Transforme en polvo cualquier sólido natural,
+                                utilizado como materia prima comestible o
+                                cosmético.
+                            </p>
+                        </div>
+                        <div class="sm:w-1/2">
+                            <img :src="productOne" class="w-80 h-auto mx-auto">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="flex flex-wrap">
+                        <div class="sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-6 mt-6">
+                                Molienda
+                            </h3>
+                            <p class="text-gray-600">
+                                Reduzca el tamaño de las partículas de sólidos
+                                naturales, comestibles y cosméticos.
                             </p>
                         </div>
                         <div class="sm:w-1/2">
