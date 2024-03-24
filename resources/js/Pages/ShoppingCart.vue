ca7b1c29cce8e4ef0d881def90924d8ac034bd3a
natanael <rojonatanael99@gmail.com>
2024-03-24 13:41:39 -0400

    Mail sending (basic)

--- resources/js/Pages/ShoppingCart.vue
+++ resources/js/Pages/ShoppingCart.vue
@@ -59,8 +59,11 @@ onMounted(() => {
             product_unit: product.unit.name,
         }
         record.value.products_info.push(productData);
-        record.value.total_price += product.price * product.product_content;
-        productsQuantity.value[index] = product.product_content;
+        if (product.type_sale.name === 'Detal') {
+            productsQuantity.value[index] = 1;
+        } else if (product.type_sale.name === 'Granel') {
+            productsQuantity.value[index] = product.product_content;
+        }
     });
     // Finalizar spinner de carga.
     isLoading.value = false;
@@ -352,8 +355,8 @@ const decreaseProductQuantity = (quantities, index) => {
                                                     <label for="product-retail-quantity">Cantidad del
                                                         producto</label>
                                                     <input type="number" id="product-retail-quantity"
-                                                        name="product-retail-quantity" :min="product.product_content"
-                                                        :max="product.stock" v-model="productsQuantity[index]" @change="record.total_price =
+                                                        name="product-retail-quantity" min="1" :max="product.stock"
+                                                        v-model="productsQuantity[index]" @change="record.total_price =
                 calculateTotalPrice(productsQuantity[index])">
                                                     <button
                                                         @click="increaseProductQuantity(productsQuantity, index)">+</button>
