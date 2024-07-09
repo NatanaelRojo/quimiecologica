ac89dbf87f3ca511cf16e715f33eb5a1d13ed3f9
Argenis Osorio <argenisosorio580@hotmail.com>
2024-07-09 10:04:22 -0400

    Reduced width Quimiecology image in landing page main section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -47,10 +47,11 @@ onMounted(async () => {
                     class="flex justify-center items-center"
                 >
                     <img
-                            :src="LogoQuimiecologi_brown"
-                            class="mb-4"
-                            alt="Quimiecologi C.A."
-                        />
+                        :src="LogoQuimiecologi_brown"
+                        class="mb-4"
+                        alt="Quimiecologi C.A."
+                        style="width: 1000px;"
+                    />
                 </div>
                 <div
                     class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
