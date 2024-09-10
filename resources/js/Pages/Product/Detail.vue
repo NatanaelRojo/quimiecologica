aaae15dcba9bf3cf2cf2b3b6406cfade40a22429
Argenis Osorio <argenisosorio580@hotmail.com>
2024-09-10 14:13:43 -0400

    Update styles

--- resources/js/Pages/Product/Detail.vue
+++ resources/js/Pages/Product/Detail.vue
@@ -53,80 +53,93 @@
 
                     <br>
 
-                    <div class="lg:w-1/2 lg:mr-8 mb-4">
-                        <img v-for="(imageUrl, index) in product.image_urls" :key="index" :src="`/storage/${imageUrl}`"
-                            alt="Imagen del producto" class="w-full h-auto img-zoom" style="
-                                padding: 20px;
-                            " />
+                    <div
+                        class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2"
+                        style="border:ridge 1px red;"
+                    >
+                        <!-- Columna izquierda -->
+                        <div style="border:ridge 1px blue;">
+                            <img
+                                v-for="(imageUrl, index) in product.image_urls"
+                                :key="index" :src="`/storage/${imageUrl}`"
+                                alt="Imagen del producto"
+                                class="h-auto"
+                                style="padding: 20px;"
+                            />
+                        </div>
+                        <!-- Final de Columna izquierda -->
+
+                        <!-- Columna derecha -->
+                        <div style="border:ridge 1px orange; padding: 10px;">
+                            <!-- Detalles del producto -->
+                            <h1 class="text-3xl font-bold mb-2">{{ product.name }}</h1>
+                            <p class="text-gray-600 mb-4 text-justify">
+                                <span v-html="product.description"></span>
+                            </p>
+                            <div class="flex space-x-2 mb-2">
+                                <span>Marca: </span>
+                                <span class="text-gray-600">
+                                    {{ product?.brand.name }}
+                                </span>
+                            </div>
+                            <div class="flex space-x-2 mb-2">
+                                <span>Clase:</span>
+                                <span class="text-gray-600">
+                                    {{ product?.primary_class.name }}
+                                </span>
+                            </div>
+                            <div class="flex space-x-2">
+                                <span>Subclase:</span>
+                                <div v-for="(category, index) of product.categories" :key="index" class="text-gray-600">
+                                    {{ category.name }}
+                                </div>
+                            </div>
+                            <div class="flex space-x-2 mt-2">
+                                <span>Cuota:</span>
+                                <div v-for="(gender, index) of product.genders" :key="index" class="text-gray-600">
+                                    {{ gender.name }}
+                                </div>
+                            </div>
+                            <div class="flex space-x-2 mt-2">
+                                <span>Presentación:</span>
+                                <div v-for="(measure, index) of product.measures" :key="index" class="text-gray-600">
+                                    <span v-if="measure.quantity > 0">
+                                        {{ measure.quantity }}
+                                    </span>
+                                    {{ measure.unit }}
+                                    {{ measure.size }}
+                                </div>
+                            </div>
+                            <div class="flex space-x-2 mt-2">
+                                <span>Tipo de venta:</span>
+                                <div class="text-gray-600">
+                                    <span class="
+                                            gradient-green
+                                            rounded-full
+                                            px-3
+                                            py-1
+                                            text-sm
+                                            text-gray-700
+                                        ">
+                                        {{ product.type_sale.name }}
+                                    </span>
+                                </div>
+                            </div>
+                            <hr class="mt-5 mb-5">
+                            <h2 class="
+                                    w-full
+                                    my-2
+                                    text-3xl
+                                    font-black
+                                    leading-tight
+                                    text-gray-800
+                                ">
+                                Precio: ${{ product.price }}
+                            </h2>
+                        </div>
+                        <!-- Final Columna derecha -->
                     </div>
 
-                    <!-- Detalles del producto -->
-                    <div class="">
-                        <h1 class="text-3xl font-bold mb-2">{{ product.name }}</h1>
-                        <p class="text-gray-600 mb-4 text-justify">
-                            <span v-html="product.description"></span>
-                        </p>
-                        <div class="flex space-x-2 mb-2">
-                            <span>Marca: </span>
-                            <span class="text-gray-600">
-                                {{ product?.brand.name }}
-                            </span>
-                        </div>
-                        <div class="flex space-x-2 mb-2">
-                            <span>Clase:</span>
-                            <span class="text-gray-600">
-                                {{ product?.primary_class.name }}
-                            </span>
-                        </div>
-                        <div class="flex space-x-2">
-                            <span>Subclase:</span>
-                            <div v-for="(category, index) of product.categories" :key="index" class="text-gray-600">
-                                {{ category.name }}
-                            </div>
-                        </div>
-                        <div class="flex space-x-2 mt-2">
-                            <span>Cuota:</span>
-                            <div v-for="(gender, index) of product.genders" :key="index" class="text-gray-600">
-                                {{ gender.name }}
-                            </div>
-                        </div>
-                        <div class="flex space-x-2 mt-2">
-                            <span>Presentación:</span>
-                            <div v-for="(measure, index) of product.measures" :key="index" class="text-gray-600">
-                                <span v-if="measure.quantity > 0">
-                                    {{ measure.quantity }}
-                                </span>
-                                {{ measure.unit }}
-                                {{ measure.size }}
-                            </div>
-                        </div>
-                        <div class="flex space-x-2 mt-2">
-                            <span>Tipo de venta:</span>
-                            <div class="text-gray-600">
-                                <span class="
-                                        gradient-green
-                                        rounded-full
-                                        px-3
-                                        py-1
-                                        text-sm
-                                        text-gray-700
-                                    ">
-                                    {{ product.type_sale.name }}
-                                </span>
-                            </div>
-                        </div>
-                        <hr class="mt-5 mb-5">
-                        <h2 class="
-                                w-full
-                                my-2
-                                text-3xl
-                                font-black
-                                leading-tight
-                                text-gray-800
-                            ">
-                            Precio: ${{ product.price }}
-                        </h2>
-                    </div>
                     <div class="flex flex-col items-center">
                         <button @click.prevent="addProductToCart(product)" class="
                                             font-montserrat
